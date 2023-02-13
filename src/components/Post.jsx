099bff1d5fbb36c01ff9445c614961a88977d1c6
Ícaro Gabriel <icarogabriel999@gmail.com>
2023-02-13 18:30:52 -0300

    Estruta HTML do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,23 @@ import styles from './Post.module.css'
 
 export function Post() {
     return (
-       <h1>Hello World</h1>
+       <article className={styles.post}>
+            <header className={styles.header}>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/XaloDev.png" alt="Post Author Picture" />
+                    <div className={styles.authorInfo}>
+                        <strong>Ícaro Xavier</strong>
+                        <span>Front-end Developer</span>
+                    </div>
+                </div>
+                <time title="13 de Fevereiro às 15:37" dateTime="2023-02-13 15:37:01">Publicado há 1h</time>
+            </header>
+            <div className={styles.content}>
+            <p>Fala galeraa 👋</p>
+            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+            <p>👉 <a href='#'>jane.design/doctorcare</a></p>
+            <p><a href='#'>#novoprojeto</a <a href='#'>#nlw</a> <a href='#'>#rocketseat</a></p>
+            </div>
+       </article>
     )
 }
