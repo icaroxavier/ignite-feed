44bf5810a3c2534d5dcf31898947210c07569842
Ícaro Gabriel <icarogabriel999@gmail.com>
2023-02-14 13:34:58 -0300

    Estilização do Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,10 +14,10 @@ export function Post() {
                 <time title="13 de Fevereiro às 15:37" dateTime="2023-02-13 15:37:01">Publicado há 1h</time>
             </header>
             <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-            <p>👉 <a href='#'>jane.design/doctorcare</a></p>
-            <p><a href='#'>#novoprojeto</a <a href='#'>#nlw</a> <a href='#'>#rocketseat</a></p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href='#'>jane.design/doctorcare</a></p>
+                <p><a href='#'>#novoprojeto</a> <a href='#'>#nlw</a> <a href='#'>#rocketseat</a></p>
             </div>
        </article>
     )
