297e1899b7daf8f273cf05c764b0a3e583937296
Ícaro Gabriel <icarogabriel999@gmail.com>
2023-02-16 02:13:59 -0300

    Estilização do comentário

---
+++ src/components/Comment.jsx
@@ -0,0 +1,33 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/XaloDev.png" alt="Comment user avatar" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Ícaro Xavier</strong>
+                            <time title="13 de Fevereiro às 15:37" dateTime="2023-02-13 15:37:01">Cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>
+                        Muito bom, parabéns!!!
+                    </p>
+                </div>
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
