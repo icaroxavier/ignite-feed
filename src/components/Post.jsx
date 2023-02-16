e3c05db1f9a03aa35d2e10afe2d4263a1ef4c085
Ícaro Gabriel <icarogabriel999@gmail.com>
2023-02-16 15:03:23 -0300

    Finalização estrutura e estilização

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
@@ -6,7 +7,7 @@ export function Post() {
        <article className={styles.post}>
             <header className={styles.header}>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/XaloDev.png" alt="Post Author Picture" />
+                    <Avatar src="https://github.com/XaloDev.png" />
                     <div className={styles.authorInfo}>
                         <strong>Ícaro Xavier</strong>
                         <span>Front-end Developer</span>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilSimpleLine } from 'phosphor-react'
+import { Avatar } from './Avatar'
 
 import styles from './Sidebar.module.css'
 
@@ -11,11 +12,7 @@ export function Sidebar() {
                 src='https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50' 
             />
             <div className={styles.profile}>
-                <img
-                    className={styles.avatar}
-                    alt='User avatar'
-                    src='https://avatars.githubusercontent.com/u/73499961?v=4'
-                ></img>
+                <Avatar src='https://github.com/XaloDev.png' />
                 <strong>Ícaro Xavier</strong>
                 <span>Front-end Developer</span>
             </div>
